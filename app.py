9e098d43a9df9a40c7f073e1843733ab11ff7eea
Samuel Knaus <samuelknaus@gmx.de>
2022-09-02 10:58:13 +0200

    Feature: Analyze jpg image and output text as html

--- app.py
+++ app.py
@@ -3,33 +3,98 @@ import os
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+import layoutparser as lp
+import cv2
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    result = ""
+    summary = ""
+
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        file_name = request.form["file_name"]
+        # pdf_tokens, pdf_images = lp.load_pdf("static/references/" + file_name, load_images=True)
+        # image = pdf_images[1]
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        image = cv2.imread("static/references/" + file_name)
+        image = image[..., ::-1]
+
+        model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
+                                         extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8],
+                                         label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"})
+        layout = model.detect(image)
+        lp.draw_box(image, layout, box_width=3)
+
+        # layoutparser.elements.Layout
+
+        text_blocks = lp.Layout([b for b in layout if b.type == 'Text'])
+        figure_blocks = lp.Layout([b for b in layout if b.type == 'Figure'])
+
+        text_blocks = lp.Layout([b for b in text_blocks
+                                 if not any(b.is_in(b_fig) for b_fig in figure_blocks)])
+
+        h, w = image.shape[:2]
+
+        left_interval = lp.Interval(0, w / 2 * 1.05, axis='x').put_on_canvas(image)
+
+        left_blocks = text_blocks.filter_by(left_interval, center=True)
+        print(type(left_blocks))
+        left_blocks.sort(key=lambda b: b.coordinates[1], inplace=True)
+
+        right_blocks = [b for b in text_blocks if b not in left_blocks]
+        print(type(right_blocks))
+        # right_blocks.sort(key=lambda b: b.coordinates[1], inplace=True)
+        right_blocks.sort(key=lambda b: b.coordinates[1])
+
+        text_blocks = lp.Layout([b.set(id=idx) for idx, b in enumerate(left_blocks + right_blocks)])
+
+        lp.draw_box(image, text_blocks,
+                    box_width=3,
+                    show_element_id=True)
+
+        ocr_agent = lp.TesseractAgent(languages='eng')
+
+        for block in text_blocks:
+            segment_image = (block
+                             .pad(left=5, right=5, top=5, bottom=5)
+                             .crop_image(image))
+
+            text = ocr_agent.detect(segment_image)
+            block.set(text=text, inplace=True)
+
+        request_text = ""
+        for txt in text_blocks.get_texts():
+            request_text += txt
+            result += "<p>" + txt + "</p>"
+
+        summary = generate_summary(request_text)
+        # return redirect(url_for("index", result=response.choices[0].text))
+
+    length = len(result.split())
+    approx_tokens = length * 1.25
+
+    return render_template("index.html", result=result, length=length, approx_tokens=approx_tokens, summary=summary)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_summary(text):
+    tldr_tag = "\n tl;dr:"
+    text += tldr_tag
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    print(text)
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=text,
+        temperature=0.1,
+        max_tokens=140,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\n"]
     )
+
+    return response.choices[0].text
