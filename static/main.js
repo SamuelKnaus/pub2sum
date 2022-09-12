17ebcd060b8b9c04d3200a48a08c92a1efc6bec8
Samuel Knaus <samuelknaus@gmx.de>
2022-09-13 00:59:28 +0200

    Improvement: Show loading animation after clicking on summarize

---
+++ static/main.js
@@ -0,0 +1,3 @@
+jQuery("#summarize-files").click(function (){
+    jQuery(this).html("<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\"></span>&nbsp;Analyzing, please wait...")
+});
