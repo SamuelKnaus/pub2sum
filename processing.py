81622cd04edea3946dfb1a29132e5c8694bebef4
Samuel Knaus <samuelknaus@gmx.de>
2023-02-06 10:20:12 +0100

    Iterate text until only one chunk is left for final summarization

--- processing.py
+++ processing.py
@@ -1,7 +1,7 @@
 import textwrap
-import pdf2doi
 
 import openai
+import pdf2doi
 from pypdf import PdfReader
 from rouge_score import rouge_scorer
 
@@ -10,6 +10,7 @@ from references import get_references
 
 
 def process_pdf_file(request, file_path):
+    print("Processing " + request.files["file"].filename)
     items = []
     item = {
         "input_text": None,
@@ -18,33 +19,43 @@ def process_pdf_file(request, file_path):
         "continuing_reference": None,
         "reference_list_entry": None
     }
-    paper = ["", "", ""]
+    input_text = ""
 
-    print("Processing PDF file...")
-
+    # Only summarize the PDF if the user decided to turn on the switch in the GUI
     if request.form.getlist("summarize_text"):
+        print("> Reading text from PDF file...")
         reader = PdfReader(file_path)
         number_of_pages = len(reader.pages)
 
+        # Loop over each page object and write the text into a variable for further processing
         for i in list(range(0, number_of_pages)):
             page = reader.pages[i]
             text = page.extract_text()
-            paper[0] = paper[0] + '\n' + text
-        item["input_text"] = paper[0]
+            input_text = input_text + '\n' + text
+        item["input_text"] = input_text
 
-        level = 1
-        while level < 3:
-            print("-Level " + str(level) + "-")
-            chunks = textwrap.wrap(paper[level - 1], 6000)
+        # Split the text into chunks that the API can handle and summarize each chunk.
+        # Iterate this procedure until only 1 chunk is left. Then create the final summary.
+        print("> Summarizing text from PDF...")
+        level = 0
+        text_summary = input_text
+        chunks = textwrap.wrap(text_summary, 6000)
+
+        while len(chunks) > 1:
+            text_summary = ""
+
             for index, chunk in enumerate(chunks):
                 print("Processing level " + str(level) + " chunk " + str(index))
                 response = get_text_summary(chunk, "text-davinci-003")
                 summary = response.choices[0].text[:-1]
-                paper[level] = paper[level] + " " + summary
-            level += 1
-        item["summary"] = paper[2] + "."
-        print("*** Final Summary ***\n" + paper[2])
+                text_summary = text_summary + " " + summary
 
+            chunks = textwrap.wrap(text_summary, 6000)
+
+        item["summary"] = text_summary + "."
+        print("*** Final Summary ***\n" + text_summary)
+
+    # Only fetch references if the user decided to turn on the switch in the GUI
     if request.form.getlist("fetch_references"):
         pdf2doi.config.set("verbose", False)
         results = pdf2doi.pdf2doi(file_path)
@@ -64,6 +75,7 @@ def process_zip_file(request, file):
 
 def get_text_summary(request_text, model):
     prompt = INSTRUCTION + "\n\n" + request_text + "###"
+
     response = openai.Completion.create(
         model=model,
         prompt=prompt,
