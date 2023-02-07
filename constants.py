fa61d141a7ed103e9832da0fb235ed52443efe76
Samuel Knaus <samuelknaus@gmx.de>
2023-02-07 11:37:28 +0100

    Bring back layoutparser for more precise summaries

--- constants.py
+++ constants.py
@@ -3,7 +3,12 @@
 """
 SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 
-ALLOWED_FILE_EXTENSIONS = {"pdf", "zip"}
+ALLOWED_FILE_EXTENSIONS = {"pdf"}
 TEMPORARY_FOLDER = "temp"
 
-INSTRUCTION = "Summarize the following scientific text."
+FIRST_INSTRUCTION = "Comprehensively summarize the following scientific text sections."
+FINAL_INSTRUCTION = "Combine the following text summaries to a comprehensive final summary of a scientific paper."
+
+LAYOUT_MODEL = "lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config"
+EXTRA_CONFIG = ["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8]
+LABEL_MAP = {0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"}
--- processing.py
+++ processing.py
@@ -1,11 +1,12 @@
 import textwrap
 
+import layoutparser as lp
+import numpy as np
 import openai
 import pdf2doi
-from pypdf import PdfReader
 from rouge_score import rouge_scorer
 
-from constants import INSTRUCTION
+from constants import FIRST_INSTRUCTION, LAYOUT_MODEL, EXTRA_CONFIG, LABEL_MAP, FINAL_INSTRUCTION
 from references import get_references
 
 
@@ -19,21 +20,12 @@ def process_pdf_file(request, file_path):
         "continuing_reference": None,
         "reference_list_entry": None
     }
-    input_text = ""
+
+    input_text = get_raw_text(file_path)
+    item["input_text"] = input_text
 
     # Only summarize the PDF if the user decided to turn on the switch in the GUI
     if request.form.getlist("summarize_text"):
-        print("> Reading text from PDF file...")
-        reader = PdfReader(file_path)
-        number_of_pages = len(reader.pages)
-
-        # Loop over each page object and write the text into a variable for further processing
-        for i in list(range(0, number_of_pages)):
-            page = reader.pages[i]
-            text = page.extract_text()
-            input_text = input_text + '\n' + text
-        item["input_text"] = input_text
-
         # Split the text into chunks that the API can handle and summarize each chunk.
         # Iterate this procedure until only 1 chunk is left. Then create the final summary.
         print("> Summarizing text from PDF...")
@@ -46,7 +38,7 @@ def process_pdf_file(request, file_path):
 
             for index, chunk in enumerate(chunks):
                 print("Processing level " + str(level) + " chunk " + str(index))
-                response = get_text_summary(input_text=chunk, model="text-davinci-003", max_tokens=300)
+                response = get_text_summary(FIRST_INSTRUCTION, input_text=chunk, model="text-davinci-003", max_tokens=400)
                 summary = response.choices[0].text[:-1]
                 text_summary = text_summary + " " + summary
 
@@ -55,11 +47,11 @@ def process_pdf_file(request, file_path):
 
         # Request final summary when only one chunk is left
         print("Processing level " + str(level) + " chunk 0")
-        response = get_text_summary(input_text=chunks[0], model="text-davinci-003", max_tokens=300)
+        response = get_text_summary(FINAL_INSTRUCTION, input_text=chunks[0], model="text-davinci-003", max_tokens=300)
         summary = response.choices[0].text[:-1]
 
         item["summary"] = summary + "."
-        print("*** Final Summary ***\n" + summary)
+        print("*** Final Summary ***" + summary)
 
     # Only fetch references if the user decided to turn on the switch in the GUI
     if request.form.getlist("fetch_references"):
@@ -75,12 +67,68 @@ def process_pdf_file(request, file_path):
     return items
 
 
+def get_raw_text(file_path):
+    input_text = ""
+
+    model = lp.Detectron2LayoutModel(LAYOUT_MODEL, extra_config=EXTRA_CONFIG, label_map=LABEL_MAP)
+    ocr_agent = lp.TesseractAgent(languages='eng')
+
+    pdf_tokens, pdf_images = lp.load_pdf(file_path, load_images=True, dpi=216)
+
+    for image in pdf_images:
+        image = np.array(image)
+        text_blocks = get_text_from_image(model, image, ocr_agent)
+
+        page_paragraphs = text_blocks.get_texts()
+
+        for page_paragraph in page_paragraphs:
+            input_text += page_paragraph
+
+    return input_text
+
+
+def get_text_from_image(model, image, ocr_agent):
+    layout = model.detect(image)
+    lp.draw_box(image, layout, box_width=3)
+
+    text_blocks = lp.Layout([b for b in layout if b.type == 'Text' or b.type == 'Title'])
+    figure_blocks = lp.Layout([b for b in layout if b.type == 'Figure'])
+    text_blocks = lp.Layout([b for b in text_blocks
+                             if not any(b.is_in(b_fig) for b_fig in figure_blocks)])
+
+    h, w = image.shape[:2]
+
+    left_interval = lp.Interval(0, w / 2 * 1.05, axis='x').put_on_canvas(image)
+    left_blocks = text_blocks.filter_by(left_interval, center=True)
+    left_blocks.sort(key=lambda b: b.coordinates[1], inplace=True)
+
+    right_blocks = [b for b in text_blocks if b not in left_blocks]
+    # right_blocks.sort(key=lambda b: b.coordinates[1], inplace=True)
+    right_blocks.sort(key=lambda b: b.coordinates[1])
+
+    text_blocks = lp.Layout([b.set(id=idx) for idx, b in enumerate(left_blocks + right_blocks)])
+
+    lp.draw_box(image, text_blocks,
+                box_width=3,
+                show_element_id=True)
+
+    for block in text_blocks:
+        segment_image = (block
+                         .pad(left=5, right=5, top=5, bottom=5)
+                         .crop_image(image))
+
+        text = ocr_agent.detect(segment_image)
+        block.set(text=text, inplace=True)
+
+    return text_blocks
+
+
 def process_zip_file(request, file):
     return "process zip"
 
 
-def get_text_summary(input_text, model, max_tokens=500):
-    prompt = INSTRUCTION + "\n\n" + "Text: ###\n"  + input_text + "\n###"
+def get_text_summary(instruction, input_text, model, max_tokens=500):
+    prompt = instruction + "\n\n" + "Text: ###\n" + input_text + "\n###"
 
     response = openai.Completion.create(
         model=model,
